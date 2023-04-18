0a1781cd763c93d6f0bd43d732b6c2f998fef68d
PaCruDaw <paulacruzado@iesmontsia.org>
2023-04-18 14:09:54 +0200

    login clase

--- php/login.php
+++ php/login.php
@@ -1,7 +1,22 @@
 <?php
-    var_dump($_POST);
-    echo "<br>";
-    echo "el correo introducido es";
-    echo $_POST["correo"];
+    //var_dump($_POST);
+    //echo "<br>";
+   
+
+    $user = $_POST["username"];
+    $pass = md5($_POST["password"]);
+
+    $usuari = "paula";
+    $contrasenya = md5("1234");
+
+    if (strcmp($user,$usuari)==0) {
+        if (strcmp($pass,$contrasenya)==0) {
+            echo "Se ha registrat correctament";
+        } else {
+            echo "S'ha produit un error";
+        }
+    } else {
+        echo "S'ha produit un error";
+    }
 
 ?>
