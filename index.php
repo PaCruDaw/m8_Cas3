6951c6572630b2d3ace4a40f6cf24c67d902ec1a
PaCruDaw <paulacruzado@iesmontsia.org>
2023-04-25 14:21:02 +0200

    login o no

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    session_start();
+   
+    if ($_SESSION) {
+        if ($_SESSION["profe"]==0){
+            header("location: php/Pagina_inicial_alumne.php");
+        } 
+        if ($_SESSION["profe"]==1){
+            header("location: php/Pagina_inicial_profe.php");
+        }     
+    } else {
+        include "html/login.html";
+    }
+    
+?>      
+    
